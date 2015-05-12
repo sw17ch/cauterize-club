ebe43638b641db41b5e1a5c9a093bbc18703c60b
John Van Enk <vanenkj@gmail.com>
2015-05-12 02:12:45 -0700

    Add error messages. Clean up a bit.

--- club/app.c
+++ club/app.c
@@ -1,6 +1,5 @@
 #include "app.h"
 #include "timeline.h"
-#include "timeline_internal.h"
 #include "hashing.h"
 #include "type_utils.h"
 #include "caut/cauterize_club.h"
@@ -17,12 +16,14 @@ int run_with_options(struct options * options) {
   struct timeline * timeline = NULL;
 
   if (timeline_ok != (tstat = timeline_init_from_file(options->datafile, &timeline))) {
+    fprintf(stderr, "Unable to load timeline from %s. (%d)\n", options->datafile, tstat);
     return tstat;
   }
 
   int result = work(&options->peer_set, timeline);
 
   if (timeline_ok != (tstat = timeline_deinit(timeline))) {
+    fprintf(stderr, "Unable to uninitialize timeline. (%d)\n", tstat);
     return tstat;
   }
 
@@ -31,27 +32,25 @@ int run_with_options(struct options * options) {
 
 static int work(struct peer_set * peer_set, struct timeline * timeline) {
   uint8_t user_buffer[VECTOR_MAX_LEN_name] = { 0 };
-  {
-    hashtype_t hash;
-    char hstr[HASH_HEX_STR_LEN];
+  hashtype_t hash;
+  char hstr[HASH_HEX_STR_LEN];
 
-    printf("Enter up to %u bytes of data.\nPress Ctrl+D when finished.\n\n", VECTOR_MAX_LEN_name);
-    size_t user_len = get_user_input(user_buffer, sizeof(user_buffer)-1);
+  printf("Enter up to %u bytes of data.\nPress Ctrl+D when finished.\n\n", VECTOR_MAX_LEN_name);
+  size_t user_len = get_user_input(user_buffer, sizeof(user_buffer)-1);
 
-    struct entry new_entry;
-    mk_participant(&new_entry.origin, "i'm a node");
-    get_time(&new_entry.timestamp);
-    mk_entry_body_beginning(&new_entry.body, (char *)user_buffer);
+  struct entry new_entry;
+  mk_participant(&new_entry.origin, "i'm a node");
+  get_time(&new_entry.timestamp);
+  mk_entry_body_beginning(&new_entry.body, (char *)user_buffer);
 
-    hash_buffer(user_buffer, user_len, hash);
-    hash_to_str(hash, hstr, sizeof(hstr));
+  hash_buffer(user_buffer, user_len, hash);
+  hash_to_str(hash, hstr, sizeof(hstr));
 
-    printf("================================================================================\n");
-    printf("User input: %s\n", user_buffer);
-    printf("User len: %lu\n", user_len);
-    printf("User hash: %s\n", hstr);
-    printf("Time: %llu\n", new_entry.timestamp);
-  }
+  printf("================================================================================\n");
+  printf("User input: %s\n", user_buffer);
+  printf("User len: %lu\n", user_len);
+  printf("User hash: %s\n", hstr);
+  printf("Time: %llu\n", new_entry.timestamp);
 
   return 0;
 }
